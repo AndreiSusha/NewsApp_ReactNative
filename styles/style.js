45034f54311e0875d4d8e03955dc7dab7f44e985
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-10 19:51:06 +0300

    feat: added global styles

---
+++ styles/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const gStyles = StyleSheet.create({
+  main: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#D1D1D1',
+  },
+  title: {
+    fontSize: 24,
+    color: '#333',
+    // fontFamily: "Roboto",
+  },
+});
